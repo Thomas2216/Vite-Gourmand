db9b623a8b8c3cae907c37a0639d6e236d030b2c
Thomas2216 <valle_thomas@hotmail.fr>
2026-02-10 20:10:59 +0100

    FRONT : Forms

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,7 @@ class UserType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('email')
-            ->add('password')
+            ->add('password', PasswordType::class, ['label' => 'Mot de passe'])
             ->add('telephone')
             ->add('adresse')
             ->add('ville')
