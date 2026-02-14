3478da17c4eb7b78e9bbb356a6eaf9fe6d6b6c11
Thomas2216 <valle_thomas@hotmail.fr>
2026-02-14 11:34:13 +0100

    correction role_employe

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -5,21 +5,18 @@ namespace App\Security;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
@@ -75,7 +72,7 @@ class UserAuthenticator extends AbstractAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        if ($this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_EMPLOYEE')) {
+        if ($this->security->isGranted('ROLE_ADMIN') || $this->security->isGranted('ROLE_EMPLOYE')) {
             $url = $this->adminUrlGenerator
                 ->setController(\App\Controller\Admin\UserCrudController::class)
                 ->setAction('index')
@@ -89,14 +86,16 @@ class UserAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        $data = [
-            // you may want to customize or obfuscate the message first
-            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
-
-            // or to translate this message
-            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
-        ];
-
-        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+//        $data = [
+//            // you may want to customize or obfuscate the message first
+//            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+//
+//            // or to translate this message
+//            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+//        ];
+//
+//        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+        $request->getSession()->set(SecurityRequestAttributes::AUTHENTICATION_ERROR, $exception);
+        return new RedirectResponse($this->urlGenerator->generate(self::LOGIN_ROUTE));
     }
 }
